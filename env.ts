7a5d1a6a11d9032f81ff50757efd5476bc7d828f
Tommm77 <picout.tom@gmail.com>
2024-06-05 20:58:20 +0200

    fix env problem

--- env.ts
+++ env.ts
@@ -4,10 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
     server: {
         POSTGRES_URL: z.string().url(),
-        GOOGLE_CLIENT_SECRET: z.string(),
         GOOGLE_CLIENT_ID: z.string(),
+        GOOGLE_CLIENT_SECRET: z.string(),
     },
-     experimental__runtimeEnv: {
-       NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-    }
+    client: {},
+    experimental__runtimeEnv: {
+        NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    },
 });
